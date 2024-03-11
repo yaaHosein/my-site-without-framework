f0e78d879945b9ed315d23a11116f2a441088f48
Yaa_hosein <layali.14.peshawar@example.com>
2024-03-11 12:03:57 +0330

    some comminted codes for animated stars and some general css styles

---
+++ site.js
@@ -0,0 +1,12 @@
+// for (let i = 1; i < 75; i++) {
+//     let stars = document.createElement('div');
+//     stars.classList.add('star');
+//     document.querySelector(".star").appendChild(stars);
+// }
+
+// function animateStars() {
+//     let AllStars = document.qq('.star');
+//     let num = Math.floor(Math.random() * AllStars.length)
+//     AllStars[num].classList.toggle('animate')
+// }
+// setInterval(animateStars, 50)
