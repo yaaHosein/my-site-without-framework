52a140d6ebe8f56372819b74f69caf412ddf4e15
Yaa_hosein <layali.14.peshawar@example.com>
2024-04-17 16:33:52 +0330

    more trying to remove the displayed text of the other button

--- site.js
+++ site.js
@@ -1,15 +1,16 @@
 const learnMoreBtn = document.querySelector('.learn-more-btn')
 const contactMeBtn = document.querySelector('.contact-me-btn')
 const text11= document.querySelector('.text11')
+const text12= document.querySelector('.text12')
 
 
 
 learnMoreBtn.addEventListener('click',(e)=>text11.classList.toggle('text11'))
-contactMeBtn.addEventListener('click', (e) => text11.classList.toggle('text11'))
+contactMeBtn.addEventListener('click', (e) => text12.classList.toggle('text12'))
 
-// let ss = learnMoreBtn.onclick
-// let bb = learnMoreBtn.onclick
+let ss = learnMoreBtn.onclick
+let bb = learnMoreBtn.onclick
 // console.log(ss);
 
-// if (ss == true) { text1.classList.add('text2') }
-if (bb == true) { text11.classList.add('text11') }
+if (ss == true) { text11.classList.add('text11') }
+// if (bb == true) { text11.classList.add('text11') }    
