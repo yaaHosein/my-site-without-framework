58d71a697a4278431812d81ee6965074b9d3b8d2
Yaa_hosein <layali.14.peshawar@example.com>
2024-04-17 12:05:25 +0330

    some stylings and cleanings

--- site.js
+++ site.js
@@ -1,24 +1,4 @@
-// const about = document.querySelector(".about");
-// const btns = document.querySelectorAll(".tab-btn");
-// const articles = document.querySelectorAll(".content");
-// about.addEventListener("click", function (e) {
-//   const id = e.target.dataset.id;
-//   if (id) {
-//     // remove selected from other buttons
-//     btns.forEach(function (btn) {
-//       btn.classList.remove("active");
-//     });
-//     e.target.classList.add("active");
-//     // hide other articles
-//     articles.forEach(function (article) {
-//       article.classList.remove("active");
-//     });
-//     const element = document.getElementById(id);
-//     element.classList.add("active");
-//   }
-// });
-
-const learnMoreBtn= document.querySelector('.leran-more-btn')
+const learnMoreBtn = document.querySelector('.learn-more-btn')
 const contactMeBtn = document.querySelector('.contact-me-btn')
 const text= document.querySelector('.text')
 const text_ = document.querySelector('.text_')
