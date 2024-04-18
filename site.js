a3518c1756ea28c5467c22e90c8e8fccbdc1de78
Yaa_hosein <layali.14.peshawar@example.com>
2024-04-18 13:40:28 +0330

    activating learn more button and adding close button

--- site.js
+++ site.js
@@ -1,16 +1,16 @@
 const learnMoreBtn = document.querySelector('.learn-more-btn')
 const contactMeBtn = document.querySelector('.contact-me-btn')
-const text11= document.querySelector('.text11')
+const text= document.querySelector('.text')
 const text12= document.querySelector('.text12')
+const closee=document.querySelector('.close')
 
-
-
-learnMoreBtn.addEventListener('click',(e)=>text11.classList.toggle('text11'))
+learnMoreBtn.addEventListener('click',(e)=>text.classList.toggle('text'))
 contactMeBtn.addEventListener('click', (e) => text12.classList.toggle('text12'))
+closee.addEventListener('click',(e)=>text.classList.add('text11'))
 
 let ss = learnMoreBtn.onclick
 let bb = learnMoreBtn.onclick
 // console.log(ss);
 
-if (ss == true) { text11.classList.add('text11') }
+// if (ss == true) { text11.classList.add('text11') }
 // if (bb == true) { text11.classList.add('text11') }    
