8d4d80512e3fa26eca9f8deb9910756ebfbf4791
Yaa_hosein <layali.14.peshawar@example.com>
2024-04-28 15:59:24 +0330

    main changes

--- site.js
+++ site.js
@@ -1,7 +1,5 @@
-const learnMoreBtn = document.querySelector('.learn-more-btn')
+
 const contactMeBtn = document.querySelector('.contact-me-btn')
-const text = document.querySelector('.text')
 const text12 = document.querySelector('.text12')
 
-learnMoreBtn.addEventListener('click', (e) => text.classList.toggle('text'))
-contactMeBtn.addEventListener('click', (e) => text12.classList.toggle('text12'))
+contactMeBtn.addEventListener('click', () => text12.classList.toggle('text12'))
